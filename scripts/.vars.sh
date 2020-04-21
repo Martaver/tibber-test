d6de016000139827fb3028f69e97d0247fce6be3
Sebastian Nemeth <sebastian@monkmage.com>
2020-04-21 16:33:18 +0300

    add branch vars

--- scripts/.vars.sh
+++ scripts/.vars.sh
@@ -10,3 +10,21 @@ if [[ -f '.env' ]]; then
 fi
 
 ROOT_DIR="${SCRIPTS_DIR}/.."
+
+function packageName() {
+    _PACKAGE_DIR=$1
+    echo $(cat $_PACKAGE_DIR/package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+}
+
+function branch() {
+
+    # If we are supplied BRANCH_NAME by GCP in a triggered build...
+    if [[ -z $BRANCH ]]; then export BRANCH=$BRANCH_NAME; fi
+
+    # If we are in a git repo, try loading branch from git directly.
+    if [[ -z $BRANCH ]]; then export BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,'); fi
+
+    echo $BRANCH
+}
+
+export BRANCH=$(branch)
